2542564370c012479a8f0b7dcfee8be2f58f36ff
Glitch (cooing-golden-begonia) <none>
2022-07-05 22:00:36 +0000

    🐏🎁 Checkpoint ./commands/public/redeemcode.js:43450757/2097

--- commands/public/redeemcode.js
+++ commands/public/redeemcode.js
@@ -9,27 +9,27 @@ module.exports = {
     .addStringOption(option => option.setName('codename').setDescription('Enter code name here').setRequired(true)),
 
 
+  
     async execute(interaction, client) {
+      const replys = client.cmdReplys;
+      await interaction.deferReply();
+      // await client.database.users.setUser(interaction.user.id);
+      let guildId = interaction.guild.id;       
+      // await client.database.servers.setGuild(guildId);        
+      const data = await client.database.servers.findOne({guildId}); 
+      // const data2 = await client.database.users.findOne({userId: interaction.user.id});
+      const codeName = interaction.options.getString("codename");
       
-    let guildId = interaction.guild.id;   
+      const code = data.find(c => c.code === codeName);
+      console.log(code)
       
-    const replys = client.cmdReplys;
-      
-    await client.database.servers.setGuild(guildId);
-      
-    await client.database.users.setUser(interaction.author.id);
-      
-    const data = await client.database.servers.findOne({guildId}); 
-      
-    const data2 = await client.database.users.findOne({userId: interaction.user.id});
-    
-    const codeName = interaction.options.getString("codename");
+      if (code) return interaction.editReply({content: 'كود غير موجود'})
+
    
-     await interaction.reply({content: 'test'})
+     
       
       
       
-const code = data.find(c => c.code === codeName);
 
 // if(!code) return interaction.reply({content: "كود غير موجود"});
 // if (code.usageBy.includes(interaction.user.id)) return await interaction.reply({content: replys.used});
