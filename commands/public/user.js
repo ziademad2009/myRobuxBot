3be26e5c5873a0628d740b481f4f94c946a0ee0f
Glitch (cooing-golden-begonia) <none>
2022-07-09 22:44:36 +0000

    🐧🙅 Checkpoint ./commands/public/user.js:43450757/526

--- commands/public/user.js
+++ commands/public/user.js
@@ -15,17 +15,19 @@ module.exports = {
       
         const username = interaction.options.getString('username');
          await noblox.getIdFromUsername(username).then(async (id) => {
-           await client.database.server.setGuild(interaction.guild.id);
-           const data =   await client.database.server.findOne({guildId: interaction.guild.id});
+           await client.database.servers.setGuild(interaction.guild.id);
+           const data = await client.database.servers.findOne({guildId: interaction.guild.id});
            const groupsArray = [];
            await noblox.getGroups(id).then(groups => {
              groups.forEach(group => {
                groupsArray.push(parseInt(group));
              })
            })
-           if (!groupsArray.includes(data.groupId)) return interaction.reply({content: `> **this user is not in the group**`, ephemeral: true})
-           
-         }).catch(e => interaction.reply({content: '> **i cant find this user in roblox**', ephemeral: true}))
+           if (!groupsArray.includes(data.groupId)) return interaction.channel.send({content: `> **this user is not in the group**`, ephemeral: true})
+          const usersData =  await pendingData.findOne({userId: id, groupId: data.groupId});
+           if (!usersData.joinTime) return interaction.reply({content: `> **look like you are in the group and you can use transfer command**`});
+           let embed = new MessageEmbed().setColor(client.embedColor).setDescription(`> **${usersData.joinTime}**`)
+         }).catch(e => console.log(e), interaction.reply({content: '> **i cant find this user in roblox**', ephemeral: true}))
     
  }
 };
