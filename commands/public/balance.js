fe61a5c5a5b5d588f32a6922be7f1d3d8ec87025
Glitch (cooing-golden-begonia) <none>
2022-07-10 14:46:26 +0000

    😄📠 Checkpoint ./commands/public/balance.js:43450757/2689

--- commands/public/balance.js
+++ commands/public/balance.js
@@ -12,10 +12,10 @@ module.exports = {
     async execute(interaction, client) {
 
     const replys = client.cmdReplys;
-      
+    const number = interaction.options.getNumber('amount');
     const guildData = await client.database.servers.findOne({guildId:interaction.guild.id});
     if (guildData && guildData.status.balance === true) return interaction.reply({content: replys.lock, ephemeral: true });
-      
+    if (!number) {
     let embed = new MessageEmbed().setColor(client.embedColor)
     let user = interaction.options.getUser('target') || interaction.user;
     if (user.bot) return interaction.reply({content: replys.bot})
@@ -25,6 +25,27 @@ module.exports = {
     if (user.id === interaction.user.id) embed.setDescription(replys.youBalance(coins));
     if (user.id !== interaction.user.id) embed.setDescription(replys.userBalane(user.username, coins))
     return interaction.reply({embeds: [embed]});
+    };
+    if (number) {
+      let user = interaction.options.getUser('target');
+      if (user.bot) return interaction.reply({content: replys.bot})
+      await client.database.users.setUser(user.id);
+      await client.database.users.setUser(interaction.user.id);
+      const data = await client.database.users.findOne({userId: interaction.user.id});
+      const data2 = await client.database.users.findOne({userId: user.id});
+      let coins = data.coins;
+      if (coins < number) return interaction.reply({content: `> **you balance is not enough **`});
+      data.coins -= number;
+      data2.coins += number;
+      data2.save();
+      data.save();
+      let embed = new MessageEmbed().setColor(client.embedColor).setDescription(`> **done transfer ${number} to <@${user.id}>**`)
+      return interaction.reply({embeds: [embed]});
+      
+    }
+
+    
+
 
 
            
