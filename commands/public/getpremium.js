d7cb4aad208a29e99d934190e4d38e38b1052662
Glitch (cooing-golden-begonia) <none>
2022-07-12 12:33:50 +0000

    🚐👍 Checkpoint ./commands/public/transfer.js:36770902/16:43450757/23 ./commands/public/getpremium.js:43450757/8

--- commands/public/getpremium.js
+++ commands/public/getpremium.js
@@ -19,8 +19,6 @@ module.exports = {
      if (!data.boostRole) return interaction.reply({content: replys.colsed}) 
      const boostRole = data.boostRole;
       
-
-      
     if (!interaction.member.roles.cache.has(boostRole)) {
       data.booster = false;
       data.save();
