6ba4ccf850b2d12d26d40bb1045dec022bbbecf3
Only-7amo <99255613+ziademad2009@users.noreply.github.com>
2022-07-16 12:23:57 -0700

    Update onJoinRequestHandle.js

--- nobloxEvents/onJoinRequestHandle.js
+++ nobloxEvents/onJoinRequestHandle.js
@@ -3,19 +3,7 @@ const data = require('../src/models/guilds');
 const {server} = require('../src/config');
 const userData = require('../src/models/pendingUsers')
 
-let date = new Date();
-date = date.toLocaleString('en-EG')
-setup()
-async function setup()  {
-const guildData = await data.findOne({guildId: '904487912408764536'});
- await noblox.setCookie(guildData.cookie).then(async result => {
-try{  
- let evt =  await noblox.onJoinRequestHandle(guildData.groupId)
- evt.on('data', async  (request) => {
- // console.log(request.requester.username);
- const id = await noblox.getIdFromUsername(request.requester.username);
- evt.emit('handle', request, true, async  () => { 
-   await userData.addUser(guildData.groupId, id, `${date}`);
+
  });
 evt.on('error', async error => {
 console.log(error)
